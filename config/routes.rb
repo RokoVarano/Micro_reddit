3a4b1a541bc7a2a310b1a49fcaf98dc71f0aea23
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-02-12 19:29:57 -0300

    Create model relationships

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  root "users#index"
+  root "posts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, shallow: true do
-    resources :posts, shallow: true do
-      resources :comments
-    end
+  resources :users do
+    resources :posts
   end
-
 end
